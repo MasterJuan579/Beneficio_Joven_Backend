f683cd8e5b89cd7e4f7b4a69c9a738fd8cf4bd42
MasterJuan579 <juanperezgutierrez579@gmail.com>
2025-09-29 22:45:52 -0600

    Fixing_Error

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'db-beneficio-joven.cduggeegs0kv.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Bn7QZb}~]rnxPw%',
+  database: 'BeneficioJoven',
   ssl: { rejectUnauthorized: false },
   connectionLimit: 10
 };
